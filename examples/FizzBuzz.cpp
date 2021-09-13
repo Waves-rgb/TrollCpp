4d00dd795047e96585c24bb8b94ba0e1bacd8327
Waves <65917727+Waves-rgb@users.noreply.github.com>
2021-09-12 22:37:12 -0400

    Update FizzBuzz.cpp

--- examples/FizzBuzz.cpp
+++ examples/FizzBuzz.cpp
@@ -1,4 +1,4 @@
-#include "TrollCpp.h"
+#include "troll++.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,8 +7,8 @@ trollar main trolling toomuchtrolling troll
 	trollxtimes trolling trollar i settroll 1 semitrolon i littletrolling 101 semitrolon i trollsomeonetrollsomeone toomuchtrolling troll
 		std::string out settroll "" semitrolon
 
-		trolliftrue trolling i trollapercentile 3 sametroll 0 toomuchtrolling out += "Fizz" semitrolon
-		trolliftrue trolling i trollapercentile 5 sametroll 0 toomuchtrolling out += "Buzz" semitrolon
+		trolliftrue trolling i trollapercentile 3 sametroll 0 toomuchtrolling out trollsomeoneby "Fizz" semitrolon
+		trolliftrue trolling i trollapercentile 5 sametroll 0 toomuchtrolling out trollsomeoneby "Buzz" semitrolon
 
 		trolliftrue trolling out.empty trolling toomuchtrolling toomuchtrolling std::cout littletrollinglittletrolling i semitrolon
 		otherwise std::cout littletrollinglittletrolling out semitrolon
@@ -16,5 +16,5 @@ trollar main trolling toomuchtrolling troll
 		std::cout littletrollinglittletrolling '\n' semitrolon
 	trolled
 
-  	std::cout littletrollinglittletrolling "Trolling complete" semitrolon
+  	std::cout littletrollinglittletrolling "\nTrolling complete\n" semitrolon
 trolled
