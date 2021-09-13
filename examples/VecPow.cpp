b1f8c9456368b7e5ab16c604e7d72798bc7856db
Waves <65917727+Waves-rgb@users.noreply.github.com>
2021-09-12 22:16:44 -0400

    idk

---
+++ examples/VecPow.cpp
@@ -0,0 +1,18 @@
+#include "troll++.h"
+#include <iostream>
+#include <vector>
+
+std::vector littletrolling trollar lotsoftrolling theAmountOfTrolling settroll troll 2 blocked 4 blocked 8 blocked 16 blocked 32 blocked 64 trolled semitrolon
+trollar count settroll 0 semitrolon
+
+trollar main trolling toomuchtrolling troll
+	trollxtimes trolling trollar x trolon theAmountOfTrolling toomuchtrolling troll
+		trollxtimes trolling trollar y trolon theAmountOfTrolling toomuchtrolling troll
+			std::cout littletrollinglittletrolling x grouptroll y littletrollinglittletrolling '\n' semitrolon
+			count trollsomeoneby x grouptroll y semitrolon
+		trolled
+	trolled
+
+  	std::cout littletrollinglittletrolling "\n\n" littletrollinglittletrolling count littletrollinglittletrolling '\n' semitrolon
+  	std::cout littletrollinglittletrolling "Trolling complete" semitrolon
+trolled
